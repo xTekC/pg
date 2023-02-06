b92935bf6a5eeace9d285e77340600224ff0ff6b
xTekC <xtekc.dev@pm.me>
2023-02-06 16:11:49 -0500

    refactor

--- src/bin/xcli/xcore.rs
+++ src/bin/xcli/xcore.rs
@@ -2,9 +2,9 @@ use passgen::xcore::chars::{r_lower, r_num, r_sym, r_upper};
 
 pub async fn xcore_main() -> String {
     let z = "".to_owned();
-    let s = r_sym().await;
     let l = r_lower().await;
-    let n = r_num().await;
     let u = r_upper().await;
-    z + s + l + n + u
+    let n = r_num().await;
+    let s = r_sym().await;
+    z + l + u + n + s
 }
